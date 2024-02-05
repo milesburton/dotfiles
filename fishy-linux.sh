5bd4b4e7ff7c986eb492a6a328fc4ac7ec9034c8
Jim Higson <jhigson@marketaxess.com>
2024-02-05 10:01:00 +0000

    non-interactive starship install

--- fishy-linux.sh
+++ fishy-linux.sh
@@ -1,12 +1,28 @@
 #! /usr/bin/sh
 
+echo "dotfile installing via apt"
+
 sudo apt-get update
 yes | sudo apt-get install tmux fish
 
-yes | curl -sS https://starship.rs/install.sh | sh
+echo "...done installing via apt"
+
+echo "dotfile installing starship..."
+
+curl -sS https://starship.rs/install.sh > starshipinstall.sh
+echo "...downloaded starship ok"
+yes | sh starshipinstall.sh
+
+echo "...done installing starship"
+
+echo "dotfile copying config..."
 
 cp ./.tmux.conf ~
 cp -R ./.config ~
 
+echo "...done copying config"
+
+echo "dotfile changing shell..."
 chsh /usr/bin/fish
+echo "...done changing shell"
 
