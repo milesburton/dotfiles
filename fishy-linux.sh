e5556785f37f651cf6b30b0f3bca2c20d8d3a12d
Jim Higson <jhigson@marketaxess.com>
2024-02-05 10:08:47 +0000

    make logging easier to spot

--- fishy-linux.sh
+++ fishy-linux.sh
@@ -1,26 +1,29 @@
 #! /usr/bin/sh
 
-echo "dotfile installing via apt"
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+HEADER="$RED[fishylinux]$NC"
+echo "$HEADER installing via apt"
 
 sudo apt-get update
 yes | sudo apt-get install tmux fish
 
 echo "...done installing via apt"
 
-echo "dotfile installing starship..."
+echo "$HEADER installing starship..."
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 echo "...done installing starship"
 
-echo "dotfile copying config..."
+echo "$HEADER copying config..."
 
 cp ./.tmux.conf ~
 cp -R ./.config ~
 
 echo "...done copying config"
 
-echo "dotfile changing shell..."
+echo "$HEADER changing shell..."
 chsh /usr/bin/fish
 echo "...done changing shell"
 
