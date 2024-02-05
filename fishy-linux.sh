9dd3a464731109c4181614372ee672bbaf0eb6ad
Jim Higson <jhigson@marketaxess.com>
2024-02-05 10:12:13 +0000

    more colourful logging

--- fishy-linux.sh
+++ fishy-linux.sh
@@ -8,22 +8,22 @@ echo "$HEADER installing via apt"
 sudo apt-get update
 yes | sudo apt-get install tmux fish
 
-echo "...done installing via apt"
+echo "$HEADER...done installing via apt"
 
 echo "$HEADER installing starship..."
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-echo "...done installing starship"
+echo "$HEADER...done installing starship"
 
 echo "$HEADER copying config..."
 
 cp ./.tmux.conf ~
 cp -R ./.config ~
 
-echo "...done copying config"
+echo "$HEADER...done copying config"
 
 echo "$HEADER changing shell..."
 chsh /usr/bin/fish
-echo "...done changing shell"
+echo "$HEADER...done changing shell"
 
