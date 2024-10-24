83cdfd4b52f013a36cf9f65cc3d147c4278f479c
Miles Burton <milesburton@users.noreply.github.com>
2024-10-24 17:38:59 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,45 +1,28 @@
-#! /usr/bin/sh
+#function starship_transient_rprompt_func
+#  starship module time
+#end
+function starship_transient_prompt_func
+  echo (starship module time)(starship module character)
+end
 
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-HEADER="$RED[fishylinux]$NC"
-echo "$HEADER installing via apt"
 
-sudo apt-get update
-yes | sudo apt-get install tmux fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
 
-echo "$HEADER...done installing via apt"
+enable_transience
 
-echo "$HEADER installing starship..."
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
-curl -sS https://starship.rs/install.sh | sh -s -- -y
-
-echo "$HEADER...done installing starship"
-
-echo "$HEADER copying config..."
-
-cp ./.tmux.conf ~
-cp -R ./.config ~
-
-echo "$HEADER...done copying config"
-
-echo "$HEADER changing shell..."
-chsh /usr/bin/fish
-echo "$HEADER...done changing shell"
-
-# Check if pnpm is installed
-if [ -x "$(command -v pnpm)" ]; then
-  # Check if package.json exists, if not, create it
-  if [ ! -f "package.json" ]; then
-    echo "$HEADER package.json not found, creating it..."
-    echo '{}' > package.json
-    echo "$HEADER...done creating package.json"
-  fi
-
-  # Install fish completion using pnpm
-  echo "$HEADER installing fish completion..."
-  pnpm install-completion fish
-  echo "$HEADER...done installing fish completion"
+if [ -d "/workspace/ui" ]; then
+  cd /workspace/ui
+  echo "Entered /workspace/ui"
+elif [ -d "/workspace/playground" ]; then
+  cd /workspace/playground
+  echo "Entered /workspace/playground"
 else
-  echo "$HEADER pnpm is not installed, skipping completion installation"
+  echo "Neither 'ui' nor 'playground' directory exists in /workspace."
 fi
