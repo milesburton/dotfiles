355ebd22d2d59b6f75f2e04033c216dc3d374438
Miles Burton <milesburton@users.noreply.github.com>
2024-10-25 11:37:16 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,25 @@
-#function starship_transient_rprompt_func
-#  starship module time
-#end
+# Transient Prompt Function (if desired)
 function starship_transient_prompt_func
   echo (starship module time)(starship module character)
 end
 
-
+# Load Starship Prompt if in Interactive Mode
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
+  starship init fish | source
 end
 
-enable_transience
+# Tabtab Source for Autocompletions
+if test -f ~/.config/tabtab/fish/__tabtab.fish
+  . ~/.config/tabtab/fish/__tabtab.fish
+end
 
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
-
-if [ -d "/workspace/ui" ]; then
+# Automatically Change to Specific Workspace Directories
+if test -d "/workspace/ui"
   cd /workspace/ui
   echo "Entered /workspace/ui"
-elif [ -d "/workspace/playground" ]; then
+else if test -d "/workspace/playground"
   cd /workspace/playground
   echo "Entered /workspace/playground"
 else
   echo "Neither 'ui' nor 'playground' directory exists in /workspace."
-fi
+end
