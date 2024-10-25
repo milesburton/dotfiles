e1d6d68e3923ba9adf62b6d8e736cfee88d0a4ea
Miles Burton <miles@milesburton.com>
2024-10-25 11:23:35 +0100

    Execute from the correct directory

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#! /usr/bin/sh
+#!/usr/bin/sh
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 HEADER="$RED[fishylinux]$NC"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"  # Get the directory of the script itself
+
 echo "$HEADER installing via apt"
 
 sudo apt-get update
@@ -18,21 +20,21 @@ echo "$HEADER...done installing starship"
 
 echo "$HEADER copying config..."
 
-cp ./.tmux.conf ~
-cp -R ./.config ~
+cp "$SCRIPT_DIR/.tmux.conf" ~
+cp -R "$SCRIPT_DIR/.config" ~
 
 echo "$HEADER...done copying config"
 
 echo "$HEADER changing shell..."
-chsh /usr/bin/fish
+chsh -s /usr/bin/fish
 echo "$HEADER...done changing shell"
 
 # Check if pnpm is installed
 if [ -x "$(command -v pnpm)" ]; then
   # Check if package.json exists, if not, create it
-  if [ ! -f "package.json" ]; then
+  if [ ! -f "$SCRIPT_DIR/package.json" ]; then
     echo "$HEADER package.json not found, creating it..."
-    echo '{}' > package.json
+    echo '{}' > "$SCRIPT_DIR/package.json"
     echo "$HEADER...done creating package.json"
   fi
 
@@ -43,3 +45,4 @@ if [ -x "$(command -v pnpm)" ]; then
 else
   echo "$HEADER pnpm is not installed, skipping completion installation"
 fi
+
