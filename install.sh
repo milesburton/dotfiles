18e6a5d2e4bea429af8215bbb90e6c08ee14e5ab
Miles Burton <milesburton@users.noreply.github.com>
2024-10-25 11:27:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ NC='\033[0m' # No Color
 HEADER="$RED[fishylinux]$NC"
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"  # Get the directory of the script itself
 
+echo "$HEADER executing script from $SCRIPT_DIR"
 echo "$HEADER installing via apt"
 
 sudo apt-get update
-yes | sudo apt-get install tmux fish
+sudo apt-get -y install tmux fish
 
 echo "$HEADER...done installing via apt"
 
@@ -28,21 +29,3 @@ echo "$HEADER...done copying config"
 echo "$HEADER changing shell..."
 chsh -s /usr/bin/fish
 echo "$HEADER...done changing shell"
-
-# Check if pnpm is installed
-if [ -x "$(command -v pnpm)" ]; then
-  # Check if package.json exists, if not, create it
-  if [ ! -f "$SCRIPT_DIR/package.json" ]; then
-    echo "$HEADER package.json not found, creating it..."
-    echo '{}' > "$SCRIPT_DIR/package.json"
-    echo "$HEADER...done creating package.json"
-  fi
-
-  # Install fish completion using pnpm
-  echo "$HEADER installing fish completion..."
-  # pnpm install-completion fish 
-  echo "$HEADER...done installing fish completion"
-else
-  echo "$HEADER pnpm is not installed, skipping completion installation"
-fi
-
